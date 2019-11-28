6b3565d2db8832ccdd1af58bd7de9881b586719f
uche <ucheobikingsley@gmail.com>
2019-11-28 15:02:57 -0800

    Toolbar and Viewpager added

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mtoolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtoolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        viewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        tabLayout = (TabLayout)findViewById(R.id.Main_tabs);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("Prototype");
+
 
     }
 }
