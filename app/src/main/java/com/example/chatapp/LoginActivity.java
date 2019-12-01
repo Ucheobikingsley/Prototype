c17169d6dc0c69271eac3c382ffc7009d7605d86
uche <ucheobikingsley@gmail.com>
2019-12-01 08:26:31 -0800

    Completed LoginActivity

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -42,6 +48,35 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void UserLogin() {
+        String Email = userEmail.getText().toString();
+        String Password = userPassword.getText().toString();
+        if(TextUtils.isEmpty(Email) && TextUtils.isEmpty(Password)){
+            Toast.makeText(this, "Please fill in the Fields", Toast.LENGTH_SHORT).show();
+        }else{
+            loadingBar.setTitle("Loggng In");
+            loadingBar.setMessage("Please wait while we log you in");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        sendUserToMainActivity();
+                        Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }else{
+                        String errMsg = task.getException().toString();
+                        Toast.makeText(LoginActivity.this, "Invalid username or password please try again" , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 
     private void initializefields() {
